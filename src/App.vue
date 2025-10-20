40b6616b7006c04b55b70f5591fa11426ba4e409
Trucker555 <14940899+trucker555@user.noreply.gitee.com>
2025-10-20 11:01:50 +0800

    添加全局默认样式；删除vue底栏信息；修改标签页信息

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,38 @@
 </template>
 
 <script lang="ts" setup>
-  //
+  import {websocketService} from "@/service/websocket";
+  import { MessageText } from "./service/message";
+  onMounted(async () => {
+    try{
+      await websocketService.connect("awdadadadwadaw");
+      console.log("尝试发送消息");
+      const testMessage = new MessageText("Hello, gin!", Date.now(), "1");
+      if(websocketService.isConnected){
+        websocketService.send(testMessage);
+      }
+    }catch(error){
+      console.log(error);
+    }
+  })
 </script>
+
+<style>
+html, body, #app,#app > * {
+  height: 100%;
+  width: 100%;
+  overflow: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+* {
+  box-sizing: border-box;
+  margin:0;
+  padding:0;
+}
+
+body::-webkit-scrollbar {
+  width: 0 !important;
+}
+</style>
